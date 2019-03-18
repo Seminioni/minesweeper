462a9b0839e39373f828445f2ca31c793750dfb1
Mikhail Semin <mikhail.semin@emlid.com>
2019-03-18 23:55:33 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+    <div id="app">
+      <mine-sweeper></mine-sweeper>
+    </div>
+</template>
+
+<script>
+  import "normalize.css";
+  import MineSweeper from "@/components/MineSweeper";
+
+  export default {
+    name: "app",
+    components: {
+      MineSweeper
+    }
+  };
+</script>
+
+<style lang="scss">
+  *:before,
+  *:after,
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    box-sizing: inherit;
+    height: 100%;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/components/MineSweeper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="minesweeper bd-left-color-white bd-top-color-white bordered">
+    <header class="minesweeper-header bd-bottom-color-white bd-right-color-white bordered">
+      <div class="bombs-left-to-find"></div>
+      <div class="funny-face"></div>
+      <div class="time-counter"></div>
+    </header>
+    <main class="field">
+      <div class="field-row">
+        <div class="field-cell"></div>
+        <div class="field-cell"></div>
+        <div class="field-cell"></div>
+        <div class="field-cell"></div>
+        <div class="field-cell"></div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .minesweeper {
+    background-color: var(--c-bg-gray);
+    min-height: 300px;
+    width: 50vw;
+    padding: 8px;
+  }
+
+  .minesweeper-header {
+    min-height: 50px;
+  }
+</style>
