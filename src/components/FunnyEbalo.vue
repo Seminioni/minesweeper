d6dca9cf0345d00cabd04cc1a0fdf21955537487
Mikhail Semin <mikhail.semin@emlid.com>
2019-03-21 01:36:32 +0300

    components: add FunnyEbalo.vue

---
+++ src/components/FunnyEbalo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="funny-ebalo"></div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss">
+  .funny-ebalo {
+    width: 26px;
+    height: 26px;
+    background: url("~@/assets/images/sprite.gif");
+    background-position: 0px 36px;
+
+    &:active {
+      background-position: 118px 36px;
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
